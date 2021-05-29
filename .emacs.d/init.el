4d46a0bf0480953387f92a45d563b855985ce522
p0txky <1000100ten@gmail.com>
2021-05-30 00:04:38 +0800

    update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,18 +1,37 @@
+;;; init.el --- Law Of Least Surprise Lattice  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021 p0txky
+
+;; Author: p0txky
+;; Keywords: Babel, Org-mode, Git
+;; Version: 1.0.0
+;; Package-Requires: ((emacs "27.1"))
+;; Homepage: https://github.com/p0txky/dotfiles/tree/main/.emacs.d
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Intuitive impersonal settings complying with the Law Of Least Surprise
+;; meant for inclusion with any initialization file especially useful to
+;; first-time Emacs users or experienced Emacs users looking for focused
+;; high-value content to copy.
+
+;;; Code:
 (org-babel-load-file
  (expand-file-name
   "config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(initial-frame-alist '((fullscreen . maximized)))
- '(package-selected-packages
-   '(panda-theme eglot rainbow-delimiters rainbow-mode use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+;;; init.el ends here
