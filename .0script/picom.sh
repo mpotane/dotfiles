62b00f8b6c5b30835d5ee241350ae7d54d8e611d
p0txky <1000100ten@gmail.com>
2021-08-14 21:39:38 +0800

    edit picom

--- .0script/picom.sh
+++ .0script/picom.sh
@@ -4,7 +4,7 @@
 killall -q picom
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+# while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
 
 # Launch picom
 picom &
