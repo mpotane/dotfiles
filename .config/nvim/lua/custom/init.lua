a847ea9640fd3be1505a82a10f4afebaad1d4742
mpotane <65337802+mpotane@users.noreply.github.com>
2023-08-11 19:57:00 +0800

    add autostart nvim-tree

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -5,3 +5,14 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+vim.opt.relativenumber = true
+
+local function open_nvim_tree()
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
